da18844b4f766bb2c38e5faf3a3cab974c565992
umeraziz45 <umeraziz4545@gmail.com>
2024-03-23 14:30:11 -0600

    collected num of times a word appears in text

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 def main():
      book_path='books/frankenstein.txt'
      text = get_book_text(book_path)
-     print(text)
+     wc = word_count(text)
+     
+     print(word_check(text))
+    
 
 def get_book_text(path):
      with open(path) as f:
         return f.read()
+
+def word_count(stuff):
+     word_list = stuff.split()
+     return len(word_list)
+
+def word_check(book):
+     book_dict = {}
+     for letter in book:
+         # print(word)
+          lowered_string = letter.lower()
+          if lowered_string in book_dict:
+               book_dict[lowered_string] += 1
+          else:
+               book_dict[lowered_string] = 1
+     return book_dict
 
 main()
