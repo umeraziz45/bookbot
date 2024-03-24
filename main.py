104cc3947c745716f8ffc5bfa7a9df7942e51562
umeraziz45 <umeraziz4545@gmail.com>
2024-03-23 18:33:51 -0600

    finished bookbot v.1

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ def main():
      text = get_book_text(book_path)
      wc = word_count(text)
      
-     print(word_check(text))
+     word_dict = word_check(text)
+     sorted_report = dict_to_list(word_dict)
+
+     print_report(sorted_report, wc)
     
 
 def get_book_text(path):
@@ -17,12 +20,37 @@ def word_count(stuff):
 def word_check(book):
      book_dict = {}
      for letter in book:
-         # print(word)
+         
           lowered_string = letter.lower()
           if lowered_string in book_dict:
                book_dict[lowered_string] += 1
           else:
                book_dict[lowered_string] = 1
      return book_dict
+
+def sort_on(dict):
+     for item in dict:
+          return dict[item]
+
+def dict_to_list(char_dict):
+     word_list = []
+     
+     for item, value in char_dict.items():
+          if item.isalpha():
+               word_list.append({item:value})
+
+     word_list.sort(reverse=True, key=sort_on) 
+
+     return word_list
+
+def print_report(sorted_list, wc):
+   print(f'--- Begin report of books/frankenstein.txt ---')
+   print(f'{wc} words found in the document')
+
+   for item in sorted_list:
+     for key, value in item.items():
+           print(f'The \"{key}\" character was found {value} times')
+   print('--- End report ---')
+
 
 main()
